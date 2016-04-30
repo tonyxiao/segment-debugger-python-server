7a290b6d52fc68c56dedf93e4cffa045f7d11d00
Tony Xiao <tonyx.ca@gmail.com>
2016-04-30 02:53:34 -0700

    Basic python API complete

--- app.py
+++ app.py
@@ -15,14 +15,29 @@ def identify():
     analytics.flush()
     
     return json.dumps({'status': 'success'})
-
+    
+@app.route('/api/track', methods=['POST'])
+def track():
+    write_key = request.args.get('writeKey')
+    params = request.get_json(True)
+    
+    analytics.write_key = write_key
+    analytics.track(params['userId'], params['event'], params['properties'])
+    analytics.flush()
+    
+    return json.dumps({'status': 'success'})
+    
+@app.route('/api/alias', methods=['POST'])
+def alias():
+    write_key = request.args.get('writeKey')
+    params = request.get_json(True)
+    
+    analytics.write_key = write_key
+    analytics.alias(params['previousId'], params['userId'])
+    analytics.flush()
+    
+    return json.dumps({'status': 'success'})
+    
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
-
-# analytics.write_key = 'dMCgkHYgAAhLeFYjG1uc46JLvohsWWRx'
-# analytics.identify('019mr8mf4r', {
-#     'email': 'john@example.com',
-#     'name': 'John Smith',
-#     'friends': 30
-# })
