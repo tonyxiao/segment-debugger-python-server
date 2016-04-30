7c6ee8386ec0883187500ce816f2b0e87e8ed0c3
Tony Xiao <tonyx.ca@gmail.com>
2016-04-30 02:51:09 -0700

    Identify endpoint is now working

--- app.py
+++ app.py
@@ -1,26 +1,24 @@
 import analytics
+import json
 import os
-from flask import Flask
-from flask import request
+from flask import Flask, request, abort
 app = Flask(__name__)
 
-@app.route('/api/identify')
+@app.route('/api/identify', methods=['POST'])
 def identify():
     write_key = request.args.get('writeKey')
+    params = request.get_json(True)
+    
     # Shouldn't have any concurrency issue since we are not multi-threading here
     analytics.write_key = write_key
-    analytics.identify('019mr8mf4r', {
-        'email': 'john@example.com',
-        'name': 'John Smith',
-        'friends': 30
-    })
+    analytics.identify(params['userId'], params['traits'])
     analytics.flush()
     
-    return 'Done'
+    return json.dumps({'status': 'success'})
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
 
 # analytics.write_key = 'dMCgkHYgAAhLeFYjG1uc46JLvohsWWRx'
 # analytics.identify('019mr8mf4r', {
